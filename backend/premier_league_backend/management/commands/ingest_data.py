f1ce147e3f6c058b82510755230daf83c2c8fab5
pxrbat <parbatbaniya1227@gmail.com>
2026-02-05 16:50:03 +0545

    Changed .CSV to .csv

--- backend/premier_league_backend/management/commands/ingest_data.py
+++ backend/premier_league_backend/management/commands/ingest_data.py
@@ -332,5 +332,5 @@ class Command(BaseCommand):
         if options.get("season"):
             self.ingest_season(options["season"], clear=clear)
         else:
-            for season in ["2017-18", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25"]:
+            for season in ["2015-16", "2016-17","2017-18","2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25"]:
                 self.ingest_season(season, clear=clear)
